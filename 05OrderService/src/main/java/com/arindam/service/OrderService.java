2807b80480982887eb1d9b34781868fa61482d4e
arindamslt <arin.saltlake@gmail.com>
2025-07-15 20:16:49 +0530

    KAFKA IMPLEMENTATION

--- 05OrderService/src/main/java/com/arindam/service/OrderService.java
+++ 05OrderService/src/main/java/com/arindam/service/OrderService.java
@@ -3,6 +3,7 @@ package com.arindam.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.arindam.model.Order;
@@ -12,9 +13,13 @@ import com.arindam.repository.OrderRepo;
 public class OrderService {
 @Autowired
 private OrderRepo orepo;
+@Autowired
+private KafkaTemplate<String,String> kafkaTemplate;
+private static final String TOPIC ="order-topic";
 public void addData(Order o)
 {
 	orepo.save(o);
+	kafkaTemplate.send(TOPIC,"Order Placed:"+o.getPname());
 }
 public List<Order> getData()
 {
